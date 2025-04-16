c23128d76b0dafbc2351ddb1b80ec5bba7392923
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-16 10:58:35 +0700

    Add files via upload

---
+++ access-array.js
@@ -0,0 +1,21 @@
+// fgo24-javascript1
+
+const data1 = [
+  [
+    "0",
+    [
+      "0",
+      "1",
+      "2",
+      {
+        string: [
+          0,
+          {
+            name: "hello",
+          },
+        ],
+      },
+    ],
+  ],
+];
+console.log(data1[0][1][3].string[1].name);
