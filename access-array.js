bd1907ab9192e16932f59c9121b5fb4935b6cef5
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-16 17:59:24 +0700

    Add files via upload

--- access-array.js
+++ access-array.js
@@ -19,3 +19,51 @@ const data1 = [
   ],
 ];
 console.log(data1[0][1][3].string[1].value);
+
+const studentData = [
+  {
+    name: "jhon",
+    score: 80,
+  },
+  {
+    name: "arie",
+    score: 99,
+  },
+  {
+    name: "denis",
+    score: 60,
+  },
+  {
+    name: "jane",
+    score: 85,
+  },
+  {
+    name: "ed",
+    score: 90,
+  },
+];
+
+// Hasil Rata-rata
+let sum = 0;
+let result = 0;
+for (let i = 0; i < studentData.length; i++) {
+  sum = sum + studentData[i].score;
+  result = sum / studentData.length;
+}
+console.log(result);
+
+// Siswa dengan nilai tertinggi
+
+let highestScore = {
+  name: "",
+  score: 0,
+};
+
+for (let a = 0; a < studentData.length; a++) {
+  if (highestScore.score < studentData[a].score) {
+    highestScore = studentData[a];
+  }
+}
+console.log(
+  `siswa dengan nama ${highestScore.name} adalah siswa dengan nilai tertinggi (${highestScore.score})`
+);
