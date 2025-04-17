2507c91fd8276712702414c9c19a5b3aa7be6757
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-17 11:55:40 +0700

    Add files via upload

--- minitask1.js
+++ minitask1.js
@@ -1,10 +1,32 @@
-function ganjilGenap(number) {
-  if (number % 2 == 0) {
-    console.log("angka yang anada masukkan adalah genap");
+// function ganjilGenap(number) {
+//   if (number % 2 == 0) {
+//     console.log("angka yang anada masukkan adalah genap");
+//   } else {
+//     console.log("angka yang anda masukkan adalah ganjil");
+//   }
+//   return;
+// }
+
+// ganjilGenap(7);
+
+const luasLingkaran = (r) => {
+  if (r % 7 == 0) {
+    console.log((22 / 7) * r * r + "adalah luas segitiga");
   } else {
-    console.log("angka yang anda masukkan adalah ganjil");
+    console.log(3.14 * r * r + "adalah luas lingkaran");
   }
   return;
-}
+};
 
-ganjilGenap(7);
+luasLingkaran(7);
+
+const kelilingLingkaran = (phi) => {
+  if (phi % 7 == 0) {
+    console.log(2 * (22 / 7) * phi + "adalah keliling segitiga");
+  } else {
+    console.log(2 * 3.14 * phi + "adalah keliling lingkaran");
+  }
+  return;
+};
+
+kelilingLingkaran(7);
