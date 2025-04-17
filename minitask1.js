a8b2589ea7c7ff1f14f4944a1c2560b4304234ec
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-17 11:35:13 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,10 @@
+function ganjilGenap(number) {
+  if (number % 2 == 0) {
+    console.log("angka yang anada masukkan adalah genap");
+  } else {
+    console.log("angka yang anda masukkan adalah ganjil");
+  }
+  return;
+}
+
+ganjilGenap(7);
